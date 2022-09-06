e62669a2d53b91b300f6c138e0cd7a557a9bfeef
shoaibshaik05 <shoaibmahaboobshaik1999@gmail.com>
2022-09-06 16:25:00 +0530

    complete the authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,17 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { getPosts } from '../api';
+import { useAuth } from '../hooks';
 import { Home, Login } from '../pages';
 import { Loader, Navbar } from './';
-
-const About = () => {
-  return <h1>About</h1>;
-};
-
-const UserInfo = () => {
-  return <h1>User</h1>;
-};
 
 const Page404 = () => {
   return <h1>404</h1>;
 };
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const auth = useAuth();
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await getPosts();
-
-      if (response.success) {
-        setPosts(response.data.posts);
-      }
-
-      setLoading(false);
-    };
-
-    fetchPosts();
-  }, []);
-
-  if (loading) {
+  if (auth.loading) {
     return <Loader />;
   }
 
@@ -45,19 +21,11 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path="/">
-            <Home posts={posts} />
+            <Home />
           </Route>
 
           <Route exact path="/login">
             <Login />
-          </Route>
-
-          <Route exact path="/about">
-            <About />
-          </Route>
-
-          <Route exact path="/user/asdasd">
-            <UserInfo />
           </Route>
 
           <Route>
@@ -70,3 +38,4 @@ function App() {
 }
 
 export default App;
+
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,7 +1,14 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
+import jwt from 'jwt-decode';
 
 import { AuthContext } from '../providers/AuthProvider';
-import { login as userLogin } from '../api';
+import { login as userLogin, register } from '../api';
+import {
+  setItemInLocalStorage,
+  LOCALSTORAGE_TOKEN_KEY,
+  removeItemFromLocalStorage,
+  getItemFromLocalStorage,
+} from '../utils';
 
 export const useAuth = () => {
   return useContext(AuthContext);
@@ -11,11 +18,42 @@ export const useProvideAuth = () => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const userToken = getItemFromLocalStorage(LOCALSTORAGE_TOKEN_KEY);
+
+    if (userToken) {
+      const user = jwt(userToken);
+
+      setUser(user);
+    }
+
+    setLoading(false);
+  }, []);
+
   const login = async (email, password) => {
     const response = await userLogin(email, password);
 
     if (response.success) {
       setUser(response.data.user);
+      setItemInLocalStorage(
+        LOCALSTORAGE_TOKEN_KEY,
+        response.data.token ? response.data.token : null
+      );
+      return {
+        success: true,
+      };
+    } else {
+      return {
+        success: false,
+        message: response.message,
+      };
+    }
+  };
+
+  const signup = async (name, email, password, confirmPassword) => {
+    const response = await register(name, email, password, confirmPassword);
+
+    if (response.success) {
       return {
         success: true,
       };
@@ -29,6 +67,7 @@ export const useProvideAuth = () => {
 
   const logout = () => {
     setUser(null);
+    removeItemFromLocalStorage(LOCALSTORAGE_TOKEN_KEY);
   };
 
   return {
@@ -36,5 +75,7 @@ export const useProvideAuth = () => {
     login,
     logout,
     loading,
+    signup,
   };
 };
+
