da8b9dc8f3dace04552662d54b467922ce4acedb
shoaibshaik05 <shoaibmahaboobshaik1999@gmail.com>
2022-09-06 14:05:28 +0530

    build login UI

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ToastProvider } from 'react-toast-notification';
 import './styles/index.css';
 import {App} from './components';
 
@@ -7,7 +8,9 @@ import {App} from './components';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ToastProvider autoDismiss autoDismissTimeout={5000} placement="top-left">
+      <App />
+    </ToastProvider>
   </React.StrictMode>
 );
 
