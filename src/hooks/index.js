90c48bd4766c980e0f4b304d5a0b94771c7d8ab8
shoaibshaik05 <shoaibmahaboobshaik1999@gmail.com>
2022-09-06 14:54:49 +0530

    more on authh

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,18 +1,40 @@
-import { useState } from "react";
+import { useContext, useState } from 'react';
+
+import { AuthContext } from '../providers/AuthProvider';
+import { login as userLogin } from '../api';
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
 
 export const useProvideAuth = () => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  const login = (email, password) => {};
+  const login = async (email, password) => {
+    const response = await userLogin(email, password);
 
-  const logout = () => {};
+    if (response.success) {
+      setUser(response.data.user);
+      return {
+        success: true,
+      };
+    } else {
+      return {
+        success: false,
+        message: response.message,
+      };
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
 
   return {
     user,
     login,
     logout,
-    loading
-  }
-
+    loading,
+  };
 };
