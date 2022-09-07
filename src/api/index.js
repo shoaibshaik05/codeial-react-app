e597a3bec4cd9175cca74a30ae421268f738e281
shoaibshaik05 <shoaibmahaboobshaik1999@gmail.com>
2022-09-07 14:49:47 +0530

    complete friends list ui

--- src/api/index.js
+++ src/api/index.js
@@ -76,3 +76,30 @@ export const fetchUserProfile = (userId) => {
     method: 'GET',
   });
 };
+
+export const fetchUserFriends = () => {
+  return customFetch(API_URLS.friends(), {
+    method: 'GET',
+  });
+};
+
+export const addFriend = (userId) => {
+  return customFetch(API_URLS.createFriendship(userId), {
+    method: 'POST',
+  });
+};
+
+export const removeFriend = (userId) => {
+  return customFetch(API_URLS.removeFriend(userId), {
+    method: 'POST',
+  });
+};
+
+export const addPost = (content) => {
+  return customFetch(API_URLS.createPost(), {
+    method: 'POST',
+    body: {
+      content,
+    },
+  });
+};
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,11 +1,31 @@
 import { useState } from 'react';
+
 import styles from '../styles/home.module.css';
+import { addPost } from '../api';
+import { useToasts } from 'react-toast-notifications';
 
 const CreatePost = () => {
   const [post, setPost] = useState('');
   const [addingPost, setAddingPost] = useState(false);
+  const { addToast } = useToasts();
 
-  const handleAddPostClick = () => {};
+  const handleAddPostClick = async () => {
+    setAddingPost(true);
+    // do some checks
+    const response = await addPost(post);
+
+    if (response.success) {
+      setPost('');
+      addToast('Post created successfully', {
+        appearance: 'success',
+      });
+    } else {
+      addToast(response.message, {
+        appearance: 'error',
+      });
+    }
+    setAddingPost(false);
+  };
 
   return (
     <div className={styles.createPost}>
